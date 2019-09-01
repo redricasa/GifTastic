31dceef9cff89c18bc30225fab66136d5aea2ef6
redricasa <fredeb26@gmail.com>
2019-09-01 16:30:57 -0700

    the null error was fixed when the variables were defined before it was called upon.

--- javascript/api.js
+++ javascript/api.js
@@ -1,34 +1,51 @@
 $(document).ready(function(){
+    var limit = 10; 
+    var APIkey = "B8y7gQpprsocHT91dIPf6DqU0dS1AQin";
+    var userSearch = "cat"; // user's search word entered here
+    var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + userSearch +"&api_key="+ APIkey+"&limit="+ limit;
+      
     $.ajax({
         url: queryUrl, 
         method: "GET"
     }).then(function(response){
         console.log(response);
     });
-var queryUrl = "http://api.giphy.com/v1/gifs/search?q=" + userSearch +"&api_key="+ APIkey+"&limit="+ limit
-var APIkey = "B8y7gQpprsocHT91dIPf6DqU0dS1AQin"
-var userSearch = "cat"; // user's search word entered here
-var limit = 10;
-// variable to store the gif searches
-var gifs = ["cats","dogs","guniea pigs"];
-function gifButtons(){
-    //deletes movie buttons to prevent duplicates
-    $("#gif-area").empty();
-    //a loop that crates buttons with same class,attribute and text of search term and adds it to the gif-area
-    for(var i=0; i< gifs.length; i++){
-        var gifButton = $("<button>");
-        gifButton.addClass("gif");
-        gifButton.attr("data-name", gifs[i]);
-        gifButton.text(gifs[i]);
-        $("#gif-area").append(gifButton);
+    
+    // variable to store the gif searches
+    var topics = ["cats","dogs","guniea pigs"];
+    function gifButtons(){
+        //deletes movie buttons to prevent duplicates
+        $("#gif-area").empty();
+        //a loop that crates buttons with same class,attribute and text of search term and adds it to the gif-area
+        for(var i=0; i< topics.length; i++){
+            var gifButton = $("<button>");
+            gifButton.addClass("gif");
+            gifButton.attr("data-name", topics[i]);
+            gifButton.text(topics[i]);
+            $("#gif-area").append(gifButton);
+        }
     }
-}
-gifButtons();
-//when the submit button is clicked, an event runs
-$("#add-gif").on("click", function(event){
-    event.preventDefault();
-    var gifSearch = $("#gif-input").val().trim();
-    gifs.push(gifSearch);
     gifButtons();
-})
-})
+    //when the submit button is clicked, an event runs
+    $("#add-gif").on("click", function(event){
+        event.preventDefault();
+        var gifSearch = $("#gif-input").val().trim();
+        topics.push(gifSearch);
+        gifButtons();
+    })
+    gifButtons();
+    //click event function to grab the object clicked and animate if still and still if animated
+    $(".gif").on("click", function(){
+        var state = $(this).attr("data-state");
+        if (state==="still"){
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        }else{
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state","still");
+        }
+
+    });
+
+
+});
