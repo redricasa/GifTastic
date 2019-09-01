7d720a1107ce7e4742e2c7cff7c8864e5ed66cd3
redricasa <fredeb26@gmail.com>
2019-08-31 23:11:35 -0700

    added a function that dynamically adds buttons after being inputted in the form through a loop

--- javascript/api.js
+++ javascript/api.js
@@ -9,18 +9,19 @@ var queryUrl = "http://api.giphy.com/v1/gifs/search?q=" + userSearch +"&api_key=
 var APIkey = "B8y7gQpprsocHT91dIPf6DqU0dS1AQin"
 var userSearch = "cat"; // user's search word entered here
 var limit = 10;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// variable to store the gif searches
+var gifs = ["cats","dogs","guniea pigs"];
+function gifButtons(){
+    //deletes movie buttons to prevent duplicates
+    $("#gif-area").empty();
+    //a loop that crates buttons with same class,attribute and text of search term and adds it to the gif-area
+    for(var i=0; i< gifs.length; i++){
+        var gifButton = $("<button>");
+        gifButton.addClass("gif");
+        gifButton.attr("data-name", gifs[i]);
+        gifButton.text(gifs[i]);
+        $("#gif-area").append(i);
+    }
+}
+gifButtons();
 })
