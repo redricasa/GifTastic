bd57377b5f849fbe6cdc5ed15a70d7b173c120d7
redricasa <fredeb26@gmail.com>
2019-09-02 19:34:37 -0700

    added a third argument to the on click functions for both the function that adds buttons to the top and the function that pauses/plays the gifs by adding a decendant selector which would be the class names for the bottons and images. took everything about the $.ajax into an .on click event handler for the class that's in the gif topic button area so that the query fires off when a button is clicked. For the inline function that maps out the location of the still and animated gifs, I replaced response.data with the name of the argument being passed(which represents value or item in the array) replaced topics[i] with the name of the argument in the forEach method. moved the on click event handler that taked user input and pushes it into the array of topics to the top since it's separate from the ajax query.

--- javascript/api.js
+++ javascript/api.js
@@ -1,43 +1,6 @@
 $(document).ready(function(){
-    var limit = 10;
-    var APIkey = "B8y7gQpprsocHT91dIPf6DqU0dS1AQin";
-    var userSearch = "cat"; // user's search word entered here
-    var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + userSearch +"&api_key="+ APIkey+"&limit="+limit;
-    
-    $.ajax({
-        url: queryUrl, 
-        method: "GET"
-    }).then(function(response){
-        //create a dynamic entry of gifs that gets the animated and still gifs to attributes
-        topics.forEach(function(data){
-            var img = $("<img>");
-            img.attr("data-still", response.data.images.fixed_height_still.url);
-            img.attr("data-animate", response.data.images.looping.mp4);
-            img.addClass("gif");
-            img.attr("src",response.data.images.fixed_height_still.url);
-            img.attr("data-state", "still")
-            data();
-        });
-        console.log(response);
-
-    });
     // variable to store the gif searches
     var topics = ["cats","dogs","guniea pigs"];
-    function gifButtons(){
-        //deletes movie buttons to prevent duplicates
-        $("#gif-area").empty();
-        //a loop that crates buttons with same class,attribute and text of search term and adds it to the gif-area
-        for(var i=0; i< topics.length; i++){
-            var gifButton = $("<button>");
-            gifButton.addClass("gif");
-            gifButton.attr("data-name", topics[i]);
-            gifButton.text(topics[i]);
-            $("#gif-area").append(gifButton);
-        }
-    }
-    //add a function that takes entry and equals it to userSearch
-
-    gifButtons();
     //when the submit button is clicked, an event runs
     $("#add-gif").on("click", function(event){
         event.preventDefault();
@@ -45,9 +8,49 @@ $(document).ready(function(){
         topics.push(gifSearch);
         gifButtons();
     })
+    
+    function gifButtons(){
+        //deletes movie buttons to prevent duplicates
+        $("#gif-area").empty();
+        //a loop that crates buttons with same class,attribute and text of search term and adds it to the gif-area
+        topics.forEach(function(data){
+            var gifButton = $("<button>");
+            gifButton.addClass("gif-topic");
+            gifButton.attr("data-name", data);
+            gifButton.text(data);
+            $("#gif-area").append(gifButton);
+        })
+    }
+    //add a function that takes entry and equals it to userSearch
     gifButtons();
+    $("#gif-area").on("click",".gif-topic", function(){   
+        var limit = 10;
+        var APIkey = "B8y7gQpprsocHT91dIPf6DqU0dS1AQin";
+        var userSearch = $(this).attr("data-name"); // user's search word entered here
+        var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + userSearch +"&api_key="+ APIkey+"&limit="+limit;
+        // var gifSearch = $(this).attr("data-gif");
+        $("#gif-display-area").empty();
+        $.ajax({
+            url: queryUrl, 
+            method: "GET"
+        }).then(function(response){
+            var resp= response.data;
+            console.log(resp);
+            //create a dynamic entry of gifs that gets the animated and still gifs to attribute
+            resp.forEach(function(data){
+                var img = $("<img>");
+                img.attr("data-still", data.images.fixed_height_still.url);
+                img.attr("data-animate", data.images.fixed_height.url);
+                img.addClass("gif");
+                img.attr("src", data.images.fixed_height_still.url);
+                img.attr("data-state", "still");
+                $("#gif-display-area").append(img);
+            });
+            console.log(response);
+        });
+    });
     //click event function to grab the object clicked and animate if still and still if animated
-    $(".gif").on("click", function(){
+    $("#gif-display-area").on("click",".gif", function(){
         var state = $(this).attr("data-state");
         if (state==="still"){
             $(this).attr("src", $(this).attr("data-animate"));
@@ -56,8 +59,5 @@ $(document).ready(function(){
             $(this).attr("src", $(this).attr("data-still"));
             $(this).attr("data-state","still");
         }
-    });
-   
-   
-    
+    });   
 });
